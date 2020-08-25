826f7b3617961dc4a371109a4c71f5272116cd3f
SeonjeongHwang <tjswjd0228@gmail.com>
2020-08-25 15:26:06 +0900

    login&signup

--- php/login.php
+++ php/login.php
@@ -1,36 +1,35 @@
 <?php
+  header('content-type: text/html; charset=utf-8');
+  // 데이터베이스 접속 문자열. (db위치, 유저 이름, 비밀번호)
+  $connect=mysqli_connect( "localhost", "pill", "pilldb", "userinfo") or die( "SQL server에 연결할 수 없습니다.");
 
-header('content-type: text/html; charset=utf-8'); 
+  mysqli_query("SET NAMES UTF8");
 
-$connect=mysqli_connect( "localhost", "pill", "pilldb", "userinfo") or 
-    die( "SQL server에 연결할 수 없습니다.");
- 
-mysqli_query("SET NAMES UTF8");
- 
-// 세션 시작
-session_start();
- 
-$id = $_POST[u_id];
-$sql = "SELECT userpw FROM person WHERE userid = '$id'";
- 
-$result = mysqli_query($connect, $sql);
- 
-// result of sql query
-if($result)
-{
+  // 세션 시작
+  session_start();
+
+  $id = $_POST['u_id'];
+  $pw = $_POST['u_pw'];
+
+  $sql = "SELECT IF(strcmp(userpw,'$pw'),0,1) pw_chk FROM person  WHERE userid = '$id'";
+
+  $result = mysqli_query($connect, $sql);
+
+  // 쿼리 결과
+  if($result)
+  {
     $row = mysqli_fetch_array($result);
-    if(is_null($row[userpw]))
+    if(is_null($row[pw_chk]))
     {
       echo "Can not find ID";
     }
     else
     {
-      echo "$row[userpw]";
+      echo "$row[pw_chk]";   // 0이면 비밀번호 불일치, 1이면 일치
     }
-}
-else
-{
+  }
+  else
+  {
    echo mysqli_errno($connect);
-}
-
+  }
 ?>
--- php/snclib_join.php
+++ php/snclib_join.php
@@ -1,8 +1,8 @@
 <?php
 
-header('content-type: text/html; charset=utf-8'); 
+header('content-type: text/html; charset=utf-8');
 
-$connect=mysqli_connect('localhost', 'pill', 'pilldb', 'userinfo') or  
+$connect=mysqli_connect('localhost', 'pill', 'pilldb', 'userinfo') or
     die( "SQL server에 연결할 수 없습니다.");
 
 mysqli_query("SET NAMES UTF8");
@@ -12,11 +12,21 @@ session_start();
 $id = $_POST[u_id];
 $pw = $_POST[u_pw];
 
-$sql = "INSERT INTO person(userid, userpw) VALUES('$id', '$pw')";
+// 기존에 등록된 아이디인지 확인 NULL : 등록되지 않은 아이디
+$sql_chk = "SELECT id FROM person WHERE userid = '$id'";
 
-$result = mysqli_query($connect, $sql);
+$result_chk = mysqli_query($connect, $sql_chk);
 
-// result of sql query
-echo mysqli_errno($connect);
+$row = mysqli_fetch_array($result_chk);
 
+if (is_null($row[id]))
+{
+    $sql = "INSERT INTO person(userid, userpw) VALUES('$id', '$pw')";
+    $result = mysqli_query($connect, $sql);
+    echo "0";
+}
+else
+{
+    echo "$row[id]";
+}
 ?>
