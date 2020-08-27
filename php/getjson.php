475abfcaf7d2e3e23abce9b48d31f55aa2d44fca
sonsuhyune <s3267s@naver.com>
2020-08-27 12:31:25 +0900

    suhyun android, mysql

---
+++ php/getjson.php
@@ -0,0 +1,33 @@
+<?php 
+
+    error_reporting(E_ALL); 
+    ini_set('display_errors',1); 
+
+    include('dbcon.php');
+        
+
+    $stmt = $con->prepare('select * from pilltest');
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0)
+    {
+        $data = array(); 
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            extract($row);
+    
+            array_push($data, 
+                array('품목일련번호'=>$품목일련번호,
+                '품목명'=>$품목명,
+                '업소일련번호'=>$업소일련번호
+            ));
+        }
+
+        header('Content-Type: application/json; charset=utf8');
+        $json = json_encode(array("webnautes"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+        echo $json;
+        
+    }
+
+?>
